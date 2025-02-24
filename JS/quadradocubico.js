7b40b0fa505a9145dc13bccd461b800496b07d23
thiago <olimpiodeoliveirathiago@gmail.com>
2025-02-24 09:13:58 -0300

    adicionei tela inicial, sistema de calculo cubico

---
+++ JS/quadradocubico.js
@@ -0,0 +1,25 @@
+// Função para converter metro quadrado para metro cúbico
+
+function converterMetroQuadradoParaCubico() {
+  // Obter os valores dos inputs
+  const a = parseFloat(document.getElementById('aInput').value);
+  const b = parseFloat(document.getElementById('bInput').value);
+  const c = parseFloat(document.getElementById('cInput').value);
+
+  // Verificar se os valores são números válidos
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert('Por favor, insira valores válidos para a, b e c');
+    return;
+  }
+
+  // Calcular o volume em metro cúbico
+  const volumeCubico = a * b * c;
+
+  // Exibir o resultado
+  const resultado = document.getElementById('resultado');
+  resultado.innerHTML = `O volume em metro cúbico é: ${volumeCubico} m³`;
+}
+
+// Adicionar evento de clique ao botão de conversão
+const botaoConverter = document.getElementById('calcular');
+botaoConverter.addEventListener('click', converterMetroQuadradoParaCubico);
